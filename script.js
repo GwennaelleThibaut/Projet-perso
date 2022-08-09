4db6e8440e8c5cfadcb4dc69d005a1b17de292df
GwennaelleThibaut <109671574+GwennaelleThibaut@users.noreply.github.com>
2022-08-09 09:02:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let menu = document.getElementById("menuContainer")
+
+function openmenu() {
+    let menu = document.getElementById("menuContainer");
+    menu.style.display = "block";
+    
+    if (checkbox.checked) {
+        menu.style.display = "block";
+        }
+    else {
+        menu.style.display = "none";
+
+    }
+}
